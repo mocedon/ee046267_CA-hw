b55f23eddb58880ffca306f50967bfdfea11e02e
mocedon <49566776+mocedon@users.noreply.github.com>
2021-06-21 18:55:13 +0300

    Update core_api.cpp
    Documentation added
    
    (Wow I actually remember what the hell I've coded last week)

--- hw4/core_api.cpp
+++ hw4/core_api.cpp
@@ -6,9 +6,18 @@
 #include <stdio.h>
 #include <vector>
 
-
+/* Class engulfing the whole multi-thread operation
+ * thread      - Represents the current working thread, each clock has only 1 active thread at most, initialized to 0
+ * clck        - Represents the clock counter
+ * inst        - Represents the instruction counter
+ * sw          - Represents thread switch event (Block) used only in blccking MT
+ * thread_num  - Represents the number of threads (active and non-active)
+ * thread_inst - Represents the current instruction on each thread, starts at 0, -1 non-active
+ * thread_halt - Represents the halt timer for each thread, for load/Store OP
+ * thread_ctxt - Represents the context of each thread separately */
 class MT {
 public:
+    // Constructor initializes everything to 0
 	MT() : thread(0), clck(0), inst(0), sw(false){
         thread_num = SIM_GetThreadsNum();
 	    thread_inst = std::vector<int>(thread_num, 0);
@@ -21,58 +30,70 @@ public:
 		}
 	}
 
-	void kill_MT() {
-	}
-
+    // Offsets the +1 on first round for fine grained MT
 	void thread_fix() {
-	    thread = -1; // offset the +1 on first round
+	    thread = -1;
 	}
 
+	// Goes over all threads checking if they are done or still active (might be idle)
     bool thread_active(){
         for (int i = 0; i < thread_num; i++){
-            if (thread_inst[i] > -1){
+            if (thread_inst[i] > -1){ // Instruction -1 is representing tread reached OP - halt
                 return true;
             }
         }
         return false;
     }
 
+    // Picks the next thread that isn't idle
     bool pick_thread(int start){
+        // start can be either 0 or 1
+        // start = 0 : blocked MT first search the current thread
+        // start = 1 : grained MT first search the next thread
         for (int i = start; i < (thread_num + start); i++){
-            int j = (thread + i) % thread_num;
-            if ((thread_inst[j] > -1) && (thread_halt[j] == 0)) {
-                if (sw && (thread != j)){
+            int j = (thread + i) % thread_num; // Constraints j to range 0 - thread_num
+            if ((thread_inst[j] > -1) && (thread_halt[j] == 0)) { // Thread is active and not on idle
+                if (sw && (thread != j)){ // Used in blocking TM only
+                    // In case that a switch event is reached and a new thread in picked we need to change context
                     for (int i = 0; i < SIM_GetSwitchCycles(); i++){
-                        update_halt();
+                        update_halt(); // For switch_cycles run the clock forward
                     }
-                    sw = false;
+                    sw = false; // Terminate the switch event
                 }
-                thread = j;
+                thread = j; // Current thread will be j
                 return true;
             }
         }
-        return false; //All threads are idle
+        return false; // All threads are idle
     }
 
+    // Set register in current thread to a curtain value
     void set_reg(int dst, int val){
 	    thread_ctxt[thread].reg[dst] = val;
 	}
 
+	// Get the value of a curtain register on current thread
 	int get_reg(int reg) {
 	    return thread_ctxt[thread].reg[reg];
 	}
 
-    void update_halt() {
+    // Moves clock forward updating idle threads
+	void update_halt() {
         clck++;
         for (int i = 0; i < thread_num; i++){
-            if (thread_halt[i] > 0){
+            if (thread_halt[i] > 0){ // Thread is idle
                 thread_halt[i]--;
             }
         }
     }
 
+    // Computes an operation
     void op(bool sw_penalty) {
-	    inst++;
+        // Blocked MT : sw_penalty = true
+        // Grained MT : sw_penalty = false
+	    inst++; // increase instruction count
+
+	    // Get the instruction for current thread
 	    Instruction curr_inst;
 	    SIM_MemInstRead((uint32_t)thread_inst[thread], &curr_inst, thread);
 	    thread_inst[thread]++;
@@ -82,47 +103,50 @@ public:
 	    int src2 = curr_inst.src2_index_imm;
 	    bool imm = curr_inst.isSrc2Imm;
 
-	    int addr;
-	    int data;
+	    int addr; // Saves the address for load/store op
+	    int data; // Saves the value for load/store op
+
 	    switch(opc) {
-	        case CMD_NOP:
+	        case CMD_NOP: // Nop - do nothing
 	            break;
-	        case CMD_ADD:
+	        case CMD_ADD: // Add r[dst] <- r[src1] + r[src2]
 	            set_reg(dst_reg, get_reg(src1) + get_reg(src2));
 	            break;
-            case CMD_ADDI:
+            case CMD_ADDI: // Add r[dst] <- r[src1] + src2
                 set_reg(dst_reg, get_reg(src1) + src2);
                 break;
-	        case CMD_SUB:
+	        case CMD_SUB: // Subtract r[dst] <- r[src1] - r[src2]
                 set_reg(dst_reg, get_reg(src1) - get_reg(src2));
 	            break;
-	        case CMD_SUBI:
+	        case CMD_SUBI: // Subtract r[dst] <- r[src1] - src2
                 set_reg(dst_reg, get_reg(src1) - src2);
 	            break;
 	        case CMD_LOAD:
-	            addr = get_reg(src1) + (imm ? src2 : get_reg(src2));
+	            addr = get_reg(src1) + (imm ? src2 : get_reg(src2)); // In case of immediate add src2
 	            SIM_MemDataRead((uint32_t)addr, &data);
 	            set_reg(dst_reg, data);
 	            thread_halt[thread] = SIM_GetLoadLat() + 1 ; // including this clock cycle
-	            sw = sw_penalty;
+	            sw = sw_penalty; // Switch event for blocked MT
 	            break;
 	        case CMD_STORE:
-                addr = get_reg(dst_reg) + (imm ? src2 : get_reg(src2));
+                addr = get_reg(dst_reg) + (imm ? src2 : get_reg(src2)); // In case of immediate add src2
                 SIM_MemDataWrite((uint32_t)addr, get_reg(src1));
                 thread_halt[thread] = SIM_GetStoreLat() + 1; // including this clock cycle
-                sw = sw_penalty;
+                sw = sw_penalty; // Switch event for blocked MT
 	            break;
 	        case CMD_HALT:
-	            thread_inst[thread] = -1;
-                sw = sw_penalty;
+	            thread_inst[thread] = -1; // Switch thread to non-active
+                sw = sw_penalty; // Switch event for blocked MT
 	            break;
 	    }
 	}
 
-    tcontext get_context(int id) {
+    // Get the context for a specific thread
+	tcontext get_context(int id) {
 	    return thread_ctxt[id];
 	}
 
+	// Calculate the CPI for the MT
     double get_CPI() {
 	    return ((double)clck) / ((double)inst);
 	}
@@ -143,13 +167,16 @@ private:
 
 };
 
+// Global MT instances
 MT *block = nullptr;
 MT *grain = nullptr;
 
+
 void CORE_BlockedMT() {
-	block = new MT();
-	while (block->thread_active()){
+	block = new MT(); // Dynamic allocation
+	while (block->thread_active()){ // All threads are active
 		if (block->pick_thread(0)){
+		    // There is a thread that is not idle
 			block->op(true);
 		}
 		block->update_halt();
@@ -157,10 +184,11 @@ void CORE_BlockedMT() {
 }
 
 void CORE_FinegrainedMT() {
-    grain = new MT();
-    grain->thread_fix();
-    while (grain->thread_active()){
+    grain = new MT(); //Dynamic allocation
+    grain->thread_fix(); // Super good enough
+    while (grain->thread_active()){ // All threads are active
         if (grain->pick_thread(1)){
+            // There is a thread that is not idle
             grain->op(false);
         }
         grain->update_halt();
@@ -169,13 +197,13 @@ void CORE_FinegrainedMT() {
 
 double CORE_BlockedMT_CPI(){
 	double ret = block->get_CPI();
-	free(block);
+	free(block); // Free allocation
     return ret;
 }
 
 double CORE_FinegrainedMT_CPI(){
     double ret = grain->get_CPI();
-    free(grain);
+    free(grain); // Free allocation
     return ret;
 }
 
